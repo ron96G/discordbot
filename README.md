821d2b7e9b60d79189dd5fe16c9545e42a0caa27
Ron Gummich <ron.gummich@telekom.de>
2021-12-27 22:45:42 +0100

    queue is now removed after 60s when the associated voice client is inactive

---
+++ README.md
@@ -0,0 +1,10 @@
+# Discordbot
+
+
+## Known Limitations
+
+- Race condition in queue when multiple songs are added at the (nearly) same time (possible fix by using a lock?!)
+- Many exceptions are not handled properly (possible fix by implementin exception handler)
+- Queue is lost when the bot connects again
+- Queue is lost when the voice client of the bot reconnects
+- Voice client in tasks is not updated when the voice client changes (thus queue is completely useless until it is removed after 1m)
--- pkg/queue.py
+++ pkg/queue.py
@@ -1,10 +1,13 @@
 import asyncio, logging
+from datetime import datetime, timedelta
 from asyncio.queues import Queue
 from asyncio.tasks import Task
 
 from discord.ext import commands
 from discord.voice_client import VoiceClient
 
+# in seconds
+VOICE_CLIENT_INACTIVITY_TIMEOUT = 60
 
 class Item():
     def __init__(self, task: Task, q: Queue):
@@ -12,6 +15,7 @@ class Item():
         self.q = q
 
     def __del__(self):
+        print('Canceling task')
         self.task.cancel()
 
 class BotQueue():
@@ -25,13 +29,15 @@ class BotQueue():
     def register(self, identifier: str, voiceClient : VoiceClient, maxsize=20):
         q = asyncio.Queue(maxsize=maxsize, loop=self.loop)
         
-        task = self.loop.create_task(self.queue_runner(queue=q, voiceClient=voiceClient))
+        task = self.loop.create_task(self.queue_runner(queue=q, voiceClient=voiceClient, identifier=identifier))
         self.log.info(f'Created queue_runner for {identifier}')
         self._queueDict[identifier] = Item(task, q)
         self.default_maxsize = maxsize
 
     def deregister(self, identifier: str):
-        del self._queueDict[identifier]
+        if self.exists(identifier):
+            self.log.info(f'Removing queue for {identifier}')
+            del self._queueDict[identifier]
 
     def exists(self, identifier: str):
         return identifier in self._queueDict
@@ -56,12 +62,22 @@ class BotQueue():
         self._queueDict[identifier].q.put_nowait(item)
         return self._queueDict[identifier].q.qsize()
 
-    async def queue_runner(self, queue: asyncio.Queue, voiceClient : VoiceClient):
+    async def queue_runner(self, queue: asyncio.Queue, voiceClient : VoiceClient, identifier: str):
         await self.bot.wait_until_ready()
         self.log.info('Started queue_runner')
 
+        not_connceted_deadline: datetime = None
+
         while not self.bot.is_closed():
-            if not voiceClient.is_playing():
+
+            if not voiceClient.is_connected():
+                if not_connceted_deadline is None:
+                    self.log.info(f'Flagged queue_runner {identifier} for inactivity')
+                    not_connceted_deadline = datetime.now() + timedelta(0, VOICE_CLIENT_INACTIVITY_TIMEOUT)
+                elif not_connceted_deadline < datetime.now():
+                    return self.deregister(identifier)
+
+            if voiceClient.is_connected() and not voiceClient.is_playing():
                 self.log.info('Getting new item from queue')
                 item = await queue.get()
                 if item is not None:
