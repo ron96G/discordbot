d88a5c8c7b17ced11fcf83add3e937c58057c5d3
Ron Gummich <ron.gummich@telekom.de>
2022-01-30 13:55:44 +0100

    updated install script

--- install.sh
+++ install.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
-set -e
+set -ex
+
+FFMPEG_VERSION="4.2.1"
+
+cd /opt
 
 yum install -y git python3 opus
 if [ ! -x "$(command -v poetry)" ]; then
-    curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
-    source $HOME/.poetry/env
+    curl -sSL https://install.python-poetry.org | python3 -
 fi
+
+export PATH="/root/.local/bin:$PATH"
+POETRY="/root/.local/bin/poetry"
+$POETRY config virtualenvs.create false
 
 if [ ! -x "$(command -v ffmpeg)" ]; then
     mkdir -p /usr/local/bin/ffmpeg
     cd /usr/local/bin/ffmpeg
-    wget https://www.johnvansickle.com/ffmpeg/old-releases/ffmpeg-4.2.1-amd64-static.tar.xz
-    tar xvf ffmpeg-4.2.1-amd64-static.tar.xz
-    mv ffmpeg-4.2.1-amd64-static/ffmpeg .
+    wget https://www.johnvansickle.com/ffmpeg/old-releases/ffmpeg-${FFMPEG_VERSION}-amd64-static.tar.xz
+    tar xvf ffmpeg-${FFMPEG_VERSION}-amd64-static.tar.xz
+    mv ffmpeg-${FFMPEG_VERSION}-amd64-static/ffmpeg .
     ln -s /usr/local/bin/ffmpeg/ffmpeg /usr/bin/ffmpeg
     cd -
 fi
 
-cd /opt
 git clone https://github.com/ron96G/discordbot.git --branch main || true
-cd discordbot
-poetry install
+cd /opt/discordbot
+$POETRY install
 chmod +x discordbot/__main__.py
 
 cat > /etc/systemd/system/discordbot.service << EOF
