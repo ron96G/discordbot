ce0bcb46ada7437eaffca05b85f9ce49d74041a0
Ron Gummich <ron.gummich@telekom.de>
2022-10-30 23:12:28 +0100

    added message when there are more than 24 tracks in the formatted track_list message

--- discordbot/cogs/utils.py
+++ discordbot/cogs/utils.py
@@ -4,6 +4,8 @@ import discord
 from audio.track import Track
 from common.context import Context
 
+MAX_FIELDS = 25
+
 
 async def reply_track_list(ctx: Context, tracks: List[Track]) -> discord.Message:
     """Returns the given tacks in a formatted format to the context channel."""
@@ -13,14 +15,25 @@ async def reply_track_list(ctx: Context, tracks: List[Track]) -> discord.Message
         color=discord.Color.dark_gold(),
     )
 
+    fields = 1
     for track in tracks:
         i = track.current
         for info in track.get_remaining_tracks():
             i += 1
-            embed.add_field(
-                name=f"Track ({i}/{track.max_len()}):",
-                value=info.pretty_print(),
-                inline=False,
-            )
+
+            if fields >= MAX_FIELDS:
+                embed.add_field(
+                    name="Too many tracks...",
+                    value=f"There are more tracks that could not be listed.",
+                    inline=False,
+                )
+                break
+            else:
+                fields += 1
+                embed.add_field(
+                    name=f"Track ({i}/{track.max_len()}):",
+                    value=info.pretty_print(),
+                    inline=False,
+                )
 
     return await ctx.send(embed=embed)
